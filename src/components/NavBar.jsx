98d717bf80c19ad127485ce5efb6e10264e2f34c
grizeus <zaytsewww@gmail.com>
2025-02-24 23:20:48 +0200

    Add NavBar component and integrate it into SharedLayout

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import { NavLink } from "react-router-dom";
+
+const cssBuilder = ({ isActive }) =>
+  ` ${isActive && "text-persian"} text-[16px] font-medium leading-5`;
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul className="flex gap-8">
+        <li>
+          <NavLink className={cssBuilder} to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className={cssBuilder} to="/catalog">
+            Catalog
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
