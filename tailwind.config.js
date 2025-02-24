a2242808894a7346dbfbe284ea86ba2428933ce1
grizeus <zaytsewww@gmail.com>
2025-02-24 23:20:40 +0200

    Set up Tailwind CSS and update project configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./src/**/*.{js,jsx,ts,tsx}"];
-export const theme = {
-  extend: {},
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "320px",
+        md: "768px",
+        lg: "1280px",
+        xl: "1440px",
+      },
+      colors: {
+        midnight: "var(--midnight)",
+        manatee: "var(--manatee)",
+        royal: "var(--royal)",
+        persian: "var(--persian)",
+        hawkes: "var(--hawkes)",
+        solitude: "var(--solitude)",
+        smoke: "var(--smoke)",
+      },
+    },
+  },
+  plugins: [],
 };
-export const plugins = [];
+
